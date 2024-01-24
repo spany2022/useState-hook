8b16fefba8032e003f556d94d950f4d71e184df6
Sandesh Pany <sandeshpany24@gmail.com>
2024-01-24 20:05:00 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
-function App() {
+const App = () => {
+  const [answer, setAnswer] = useState("Guess Which React Hook is used here?");
+  let clickForAnswer = () =>{
+    setAnswer("useState");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{answer}</h1>
+      <button className="btn" onClick={clickForAnswer}>Click me to know the answer</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
